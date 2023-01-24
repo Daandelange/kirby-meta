ae18599eea0f517b0c9b21a08560091a569a7ed7
Daan <idaany@gmail.com>
2023-01-24 22:03:41 +0100

    Implement Alternate language URLs #26

--- snippets/general.php
+++ snippets/general.php
@@ -7,3 +7,30 @@
 <?php if ($themeColor = \FabianMichael\Meta\Helper::themeColor()): ?>
     <meta name="theme-color" content="<?= esc($themeColor, 'attr') ?>">
 <?php endif ?>
+
+<?php
+if (kirby()->multilang() && kirby()->languages()->count() > 1) {
+    foreach (kirby()->languages() as $lang) {
+        // Note: x-default defines the entry page for unlisted languages. It doesn't define the default language.
+		if ($lang->isDefault()) {
+            echo Kirby\Toolkit\Html::tag('link', null, [
+                'rel' => 'alternate',
+                'hreflang' => 'x-default',
+                'href' => \FabianMichael\Meta\Helper::urlForLanguageDetection($page),
+            ])."\n";
+        }
+        // Don't provide alternate for current lang
+        if ($lang->code() === kirby()->language()->code()) {
+            continue;
+        }
+
+        // Only provide links for pages that have actual translation content
+        if ($page->translation($lang->code())->exists() === true) {
+            echo Kirby\Toolkit\Html::tag('link', null, [
+                'rel' => 'alternate',
+                'hreflang' => $lang->locale(LC_ALL),
+                'href' => $page->url($lang->code()),
+            ])."\n";
+        }
+    }
+}
--- src/Helper.php
+++ src/Helper.php
@@ -3,6 +3,7 @@
 namespace FabianMichael\Meta;
 
 use Kirby\Cms\App;
+use Kirby\Cms\Page;
 
 class Helper
 {
@@ -30,5 +31,21 @@ class Helper
         return kirby()->apply('meta.theme.color', [
             'color' => option('fabianmichael.meta.theme.color'),
         ], 'color');
+    }
+
+    public static function urlForLanguageDetection(Page $page): string
+    {
+        // Use kirby's automatic language chooser
+        // Note: languages.detect is incompatible with custom language urls
+        $isUsingCustomUrls = kirby()->language()->url() === kirby()->url();
+        $languageDetectionIsEnabled = kirby()->option('languages.detect', false) === true;
+        if ($languageDetectionIsEnabled && $isUsingCustomUrls) {
+            return $page->urlForLanguage('');
+        }
+        else {
+            // Ideally provide an auto-select link, here we provide a link to the "most common" language
+            $defaultInternationalLang = kirby()->languages()->has('en') ? kirby()->language('en') : kirby()->defaultLanguage();
+            return $page->url( $defaultInternationalLang->code() );
+        }
     }
 }
